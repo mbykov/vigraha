fc9785ce9ae8ddd720c5d7d98af9d7052a360d48
Michael Bykov <m.bykov@gmail.com>
2016-02-04 14:51:55 +0300

    i

--- index.js
+++ index.js
@@ -24,29 +24,83 @@ function flakes() {
   получает что? stems или tail?
   // scraper должет возвращать массив {pos: number, flake: 'string', vows: vows, sutra: sutra}
   пока только {pos:0, flakes: ['a', 'ab', 'abc']}
+
+  ==============
+  вопрос: что откидывать? tail должен начинаться с согласной?
+
+
 */
 
-flakes.prototype.scrape = function(samasa) {
-    var syms = samasa.split('');
-    log('syms', syms);
-    var start = 0;
-    var size = 1;
-    var vows = 0;
-    var flake, flakearr, beg;
-    while (vows <= 5) {
-        flakearr = syms.slice(start, size);
-        beg = flakearr[0];
-        flake = flakearr.join('');
-        log('F', flake)
-        size++;
-        vows++;
+flakes.prototype.scrape = function(rawsamasa) {
+    var total = rawsamasa.length;
+    var flakes = [];
+    // log('syms', totsyms);
+    var samasa = rawsamasa;
+    var pos = 0;
+    var beg;
+    while (pos < total) {
+        samasa = rawsamasa.slice(pos);
+        beg = u.first(samasa);
+        pos++;
+        if (!u.isConsonant(beg) && !u.isVowel(beg)) continue;
+        log('S', pos, samasa);
+        flakes.push(cutTail(samasa, pos));
     }
-    return true;
+    return flakes;
 }
 
 
+function cutTail(samasa, pos) {
+    var flakes = [];
+    var flakesize = 0;
+    var vows = 0;
+    var flake, rawtail, res;
+    var firsts = [];
+    while (vows < 5) {
+        flakesize++;
+        vows++;
+        flake = samasa.slice(0, flakesize);
+        if (flake == samasa) continue;
+        // log('FLAKE', 'pos', pos, 's', samasa.length, 'fsize', flakesize, 's.size+f.size',  samasa.length+flakesize, 'fl', flake)
+        vows = vowCount(flake);
+        // log('V', vows);
+        rawtail = samasa.slice(flakesize);
+        beg = rawtail[0];
+        // log('====== F', flake, 'T', rawtail, 'B', beg, '========');
+        if (!u.isConsonant(beg)) continue;
+        res = sandhi.del(samasa, rawtail);
+        // // log('R', res)
+        // if (res.length == 0) continue;
+        // // res.forEach(function(r) { r.flake = flake});
+        // firsts = res.map(function(r) { return r.firsts});
+        // firsts = _.uniq(_.flatten(firsts));
+        // // log('FF', firsts)
+        // flakes.push({pos: pos, flakes: firsts});
+    }
+    return flakes;
+}
 
 
+function vowCount(flake) {
+    var syms = flake.split('');
+    var beg = syms[0];
+    var vows = (u.isVowel(beg)) ? 1 : 0;
+    syms.forEach(function(s) {
+        if (u.isConsonant(s)) vows+=1;
+        else if (s == c.virama) vows-=1;
+    });
+    return vows;
+}
+
+function _vowCount(str) {
+    var syms = str.split('');
+    var vows = (u.c(c.allvowels, syms[0])) ? 1 : 0;
+    syms.forEach(function(s) {
+        if (u.c(c.hal, s)) vows+=1;
+        else if (c.virama == s) vows-=1;
+    });
+    return vows;
+}
 
 
 
