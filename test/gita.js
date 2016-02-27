2324c1f3dd8875808889570d82a0d1e0cf11aef9
Michael Bykov <m.bykov@gmail.com>
2016-02-27 16:19:10 +0300

    i

--- test/gita.js
+++ test/gita.js
@@ -28,7 +28,7 @@ runGitaTests();
 function runGitaTests() {
     getDocs(function(docs) {
         // var cleans = cleaner(docs)
-        docs = docs.slice(345);
+        docs = docs.slice(400);
         docs.forEach(function(doc, idx) {
             // p(doc);
             log('IDX', idx, 'sutra:', doc.num, '_ID', doc._id);
